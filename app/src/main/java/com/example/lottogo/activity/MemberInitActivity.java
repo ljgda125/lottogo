f2af19da58db951befc714cea21a2141e9ea58a8
choi <eunnnnn0629@gmail.com>
2020-06-01 23:15:47 +0900

    Initial commit

---
+++ app/src/main/java/com/example/lottogo/activity/MemberInitActivity.java
@@ -0,0 +1,182 @@
+package com.example.lottogo.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+
+import com.bumptech.glide.Glide;
+import com.example.lottogo.R;
+import com.example.lottogo.view.MemberInfo;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class MemberInitActivity extends BasicActivity {
+    private static final String TAG = "MemberInitActivity";
+    private ImageView profileImageVIew;
+    private RelativeLayout loaderLayout;
+    private String profilePath;
+    private FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_init);
+
+        loaderLayout = findViewById(R.id.loaderLyaout);
+        profileImageVIew = findViewById(R.id.profileImageView);
+        profileImageVIew.setOnClickListener(onClickListener);
+
+        findViewById(R.id.checkButton).setOnClickListener(onClickListener);
+        findViewById(R.id.picture).setOnClickListener(onClickListener);
+        findViewById(R.id.gallery).setOnClickListener(onClickListener);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 0: {
+                if (resultCode == Activity.RESULT_OK) {
+                    profilePath = data.getStringExtra("profilePath");
+                    Glide.with(this).load(profilePath).centerCrop().override(500).into(profileImageVIew);
+                }
+                break;
+            }
+        }
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.checkButton:
+                    storageUploader();
+                    break;
+                case R.id.profileImageView:
+                    CardView cardView = findViewById(R.id.buttonsCardView);
+                    if (cardView.getVisibility() == View.VISIBLE) {
+                        cardView.setVisibility(View.GONE);
+                    } else {
+                        cardView.setVisibility(View.VISIBLE);
+                    }
+                    break;
+                case R.id.picture:
+                    myStartActivity(CameraActivity.class);
+                    break;
+                case R.id.gallery:
+                    myStartActivity(GalleryActivity.class);
+                    break;
+            }
+        }
+    };
+
+    private void storageUploader() {
+        final String name = ((EditText) findViewById(R.id.nameEditText)).getText().toString();
+        final String phoneNumber = ((EditText) findViewById(R.id.phoneNumberEditText)).getText().toString();
+        final String birthDay = ((EditText) findViewById(R.id.birthDayEditText)).getText().toString();
+        final String address = ((EditText) findViewById(R.id.addressEditText)).getText().toString();
+
+        if (name.length() > 0 && phoneNumber.length() > 9 && birthDay.length() > 5 && address.length() > 0) {
+            loaderLayout.setVisibility(View.VISIBLE);
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference storageRef = storage.getReference();
+            user = FirebaseAuth.getInstance().getCurrentUser();
+            final StorageReference mountainImagesRef = storageRef.child("users/" + user.getUid() + "/profileImage.jpg");
+
+            if (profilePath == null) {
+                MemberInfo memberInfo = new MemberInfo(name, phoneNumber, birthDay, address);
+                storeUploader(memberInfo);
+            } else {
+                try {
+                    InputStream stream = new FileInputStream(new File(profilePath));
+                    UploadTask uploadTask = mountainImagesRef.putStream(stream);
+                    uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                        @Override
+                        public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                            if (!task.isSuccessful()) {
+                                throw task.getException();
+                            }
+                            return mountainImagesRef.getDownloadUrl();
+                        }
+                    }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            if (task.isSuccessful()) {
+                                Uri downloadUri = task.getResult();
+
+                                MemberInfo memberInfo = new MemberInfo(name, phoneNumber, birthDay, address, downloadUri.toString());
+                                storeUploader(memberInfo);
+                            } else {
+                                startToast("회원정보를 보내는데 실패하였습니다.");
+                            }
+                        }
+                    });
+                } catch (FileNotFoundException e) {
+                    Log.e("로그", "에러: " + e.toString());
+                }
+            }
+        } else {
+            startToast("회원정보를 입력해주세요.");
+        }
+    }
+
+    private void storeUploader(MemberInfo memberInfo) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("users").document(user.getUid()).set(memberInfo)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        startToast("회원정보 등록을 성공하였습니다.");
+                        loaderLayout.setVisibility(View.GONE);
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        startToast("회원정보 등록에 실패하였습니다.");
+                        loaderLayout.setVisibility(View.GONE);
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+    }
+
+    private void startToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private void myStartActivity(Class c) {
+        Intent intent = new Intent(this, c);
+        startActivityForResult(intent, 0);
+    }
+}
---
+++ app/src/main/java/com/example/lottogo/adapter/MainAdapter.java
@@ -0,0 +1,91 @@
+package com.example.lottogo.adapter;
+
+import android.app.Activity;
+import android.util.Patterns;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.lottogo.R;
+import com.example.lottogo.view.PostInfo;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.GalleryViewHolder> {
+    private ArrayList<PostInfo> mDataset;
+    private Activity activity;
+
+    static class GalleryViewHolder extends RecyclerView.ViewHolder {
+        CardView cardView;
+        GalleryViewHolder(CardView v) {
+            super(v);
+            cardView = v;
+        }
+    }
+
+    public MainAdapter(Activity activity, ArrayList<PostInfo> myDataset) {
+        mDataset = myDataset;
+        this.activity = activity;
+    }
+
+    @NonNull
+    @Override
+    public MainAdapter.GalleryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        CardView cardView = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.item_post, parent, false);
+        final GalleryViewHolder galleryViewHolder = new GalleryViewHolder(cardView);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return galleryViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final GalleryViewHolder holder, int position) {
+        CardView cardView = holder.cardView;
+        TextView titleTextView = cardView.findViewById(R.id.titleTextView);
+        titleTextView.setText(mDataset.get(position).getTitle());
+
+        TextView createdAtTextView = cardView.findViewById(R.id.createAtTextView);
+        createdAtTextView.setText(new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(mDataset.get(position).getCreatedAt()));
+
+        LinearLayout contetnsLayout = cardView.findViewById(R.id.contentsLayout);
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        ArrayList<String> contentsList = mDataset.get(position).getContents();
+
+        if(contetnsLayout.getChildCount() == 0){
+            for (int i = 0; i < contentsList.size(); i++ ){
+                String contents = contentsList.get(i);
+                if(Patterns.WEB_URL.matcher(contents).matches()){
+                    ImageView imageView = new ImageView(activity);
+                    imageView.setLayoutParams(layoutParams);
+                    contetnsLayout.addView(imageView);
+                    Glide.with(activity).load(contents).override(1000).into(imageView);
+                }else {
+                    TextView textView = new TextView(activity);
+                    textView.setLayoutParams(layoutParams);
+                    textView.setText(contents);
+                    contetnsLayout.addView(textView);
+                }
+            }
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
